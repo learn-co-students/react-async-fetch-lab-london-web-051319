5894ecd121aa54367b61dab51a60078974463ed3
moniet <moniet.sawhney@gmail.com>
2019-07-19 15:56:33 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+    };
+  }
+
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(res => res.json())
+      .then(data => this.setState({data}))
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <h2>people</h2>
+        <ul>
+          { data ? data.people.map(person => <li>{ person.name }</li>) : <div></div> }
+        </ul>
+      </div>
+    );
+  }
+}
